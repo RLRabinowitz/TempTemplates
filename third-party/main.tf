9b65f0c2b2805a656167479b14a304a75095e54c
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2020-09-15 12:16:49 +0300

    Update main.tf

--- third-party/main.tf
+++ third-party/main.tf
@@ -1,3 +1,9 @@
-provider "elasticsearch" {
-    version = "1.0.0"
- }
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    elasticsearch = {
+      source = "my-proj/my-inner/elasticsearch"
+      version = "1.0.0"
+    }
+  }
+}

a6a4ed22fcb270b2ae2dbda08381bf25988c021f
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2020-12-03 13:13:06 +0200

    Update my_buckets.tf

--- module-13/my_buckets.tf
+++ module-13/my_buckets.tf
@@ -1,6 +1,6 @@
 # my_buckets.tf
 module "bucket" {
-  for_each = toset(["assets", "media"])
+  for_each = toset(["arel_temp_assets", "arel_temp_media"])
   source   = "./publish_bucket"
   name     = "${each.key}_bucket"
 }

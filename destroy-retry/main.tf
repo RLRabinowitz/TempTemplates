af22ab6cc9fe219a1c33cd5304772ff06dd74486
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2020-12-14 12:10:06 +0200

    Create main.tf

---
+++ destroy-retry/main.tf
@@ -0,0 +1,12 @@
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_10_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "10s"
+}
+
+# This resource will create (at least) 10 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_10_seconds]
+}

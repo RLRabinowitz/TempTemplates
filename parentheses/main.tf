ed4f6bbb52397a5971bcf7f53ca7074102c5bbbd
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2020-12-29 19:01:05 +0200

    Update main.tf

--- parentheses/main.tf
+++ parentheses/main.tf
@@ -1,11 +1,4 @@
-resource "google_pubsub_topic" "audit_topic" {
-  metadata {
-    name = "terraform-example"
-  }
-  spec {
-    replicas = var.pre_migration_scale_down_mode_enabled ? 0 : (var.minimal_setup_enabled ? local.minimal_setup_replicas : 1)
-    strategy {
-      type = local.kubernetes_deployment_strategy_rolling_update
-    }
-  }
+resource "aws_s3_bucket" "example" {
+  bucket = "arel-temp-parentheses"
+  acl    = (false ? "public-read" : "private")
 }

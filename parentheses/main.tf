cdc4cef68f0d69c1ff42abea0116a9c82b7915af
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2020-12-29 19:03:47 +0200

    Update main.tf

--- parentheses/main.tf
+++ parentheses/main.tf
@@ -1,4 +1,11 @@
 resource "aws_s3_bucket" "example" {
   bucket = "arel-temp-parentheses"
   acl    = (false ? "public-read" : "private")
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://env0.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 }

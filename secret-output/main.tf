96a2d404d42f8b3185931bdd66a07ebc7fbcee9d
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2021-01-31 13:54:24 +0200

    Create main.tf

---
+++ secret-output/main.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "null_1" {
+    
+}
+
+output "cool" {
+  value     = "very"
+}
+
+output "sod" {
+  value     = "secret"
+  sensitive = true
+}
+
+output "kamus" {
+  value     = "shhhhhh"
+  sensitive = true
+}

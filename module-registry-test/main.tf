a2fe2742e71026db797bd8edcdca78613af08239
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2021-12-15 10:11:40 +0200

    Update main.tf

--- module-registry-test/main.tf
+++ module-registry-test/main.tf
@@ -2,11 +2,3 @@ module "binary-authorization" {
   source = "api-dev.dev.env0.com/9e4c5088-720d-4912-98a3-8b0c7e904a0f/binary-authorization/vmo2-gitlab-shared"
   version = "1.0"
 }
-
-credentials "api-dev.dev.env0.com" {
-  # A valid Basic authentication header value, using your env0 API Key ID and Secret
-  token = "Basic base64-encoded(var.API_KEY)"
-}
-  
-variable "API_KEY" {
-}

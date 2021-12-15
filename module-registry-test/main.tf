30daff03aa51e288b160740ee819462f398c7cf7
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2021-12-15 10:22:55 +0200

    Update main.tf

--- module-registry-test/main.tf
+++ module-registry-test/main.tf
@@ -1,4 +1,4 @@
 module "binary-authorization" {
-  source = "api-dev.dev.env0.com/a/b/c" # "api-dev.dev.env0.com/9e4c5088-720d-4912-98a3-8b0c7e904a0f/binary-authorization/vmo2-gitlab-shared"
+  source = "api-dev.dev.env0.com/9e4c5088-720d-4912-98a3-8b0c7e904a0f/b/c" # "api-dev.dev.env0.com/9e4c5088-720d-4912-98a3-8b0c7e904a0f/binary-authorization/vmo2-gitlab-shared"
   version = "1.0"
 }

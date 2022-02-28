55358b558a821fafeffb729e69e25c00ce326581
RLRabinowitz <rlrabinowitz2@gmail.com>
2022-02-28 17:05:59 +0200

    Do create all resources

--- k8s-module/main.tf
+++ k8s-module/main.tf
@@ -1,28 +1,6 @@
 module "prereq" {
   source = "github.com/env0/k8s-modules//aws"
 
-  modules_info = {
-    vpc = {
-      create = false
-      id = var.vpc_id
-      private_subnets = ["172.16.0.0/21", "172.16.16.0/21", "172.16.32.0/21", "172.16.48.0/21", "172.16.64.0/21"]
-    }
-    efs = {
-      create = false
-      id = var.efs_id
-    },
-    autoscaler = {
-      create = true
-    }
-    csi_driver = {
-      create = true
-    }
-    eks = {
-      create = true
-      cluster_id = "kaka"
-    }
-  }
-
   region = "us-east-1"
   cluster_name = "arel-shld-cluster"
 }

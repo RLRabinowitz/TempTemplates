12541656dee047c9a3d2c723b98c364b0b028693
RLRabinowitz <rlrabinowitz2@gmail.com>
2022-02-28 15:34:02 +0200

    Create template for k8s module with existing vpc and efs

---
+++ k8s-module/main.tf
@@ -0,0 +1,38 @@
+module "prereq" {
+  source = "github.com/env0/k8s-modules//aws"
+
+  modules_info = {
+    vpc = {
+      create = false
+      id = var.vpc_id
+    }
+    efs = {
+      create = false
+      id = var.efs_id
+    }
+  }
+
+  region = "us-east-1"
+  cluster_name = "arel-shld-cluster"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "helm_release" "agent" {
+  depends_on       = [module.prereq] // add all necessary dependencies here
+  name             = "env0-agent-arel"
+  namespace        = "env0-agent-arel"
+  chart            = "env0-agent"
+  create_namespace = true
+  repository       = "https://env0.github.io/self-hosted"
+  timeout          = 600
+  values           = [
+    yamlencode(var.env0_values)
+  ]
+}
+
+variable "env0_values" {}
+variable "vpc_id" {}
+variable "efs_id" {}

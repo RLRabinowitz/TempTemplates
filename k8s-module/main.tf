216d9d106689c97e9d9557f81e8f8e9c213e40d4
RLRabinowitz <rlrabinowitz2@gmail.com>
2022-02-28 18:13:20 +0200

    Set up providers

--- k8s-module/main.tf
+++ k8s-module/main.tf
@@ -22,6 +22,39 @@ resource "helm_release" "agent" {
   ]
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.prereq.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.prereq.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
+}
+
 variable "env0_values" {}
 variable "vpc_id" {}
 variable "efs_id" {}

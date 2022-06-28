afa1ea6d25adf420ba83abddf28ff78df7e41e16
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2022-06-28 18:37:36 +0300

    Update index.ts

--- pulumi/aws/hello-world/index.ts
+++ pulumi/aws/hello-world/index.ts
@@ -1,17 +1,17 @@
-import * as apigateway from "@pulumi/aws-apigateway";
-import * as fs from 'fs';
+// import * as apigateway from "@pulumi/aws-apigateway";
+// import * as fs from 'fs';
 
-const indexTemplate = fs.readFileSync('index.template.html', 'utf-8');
-const index = indexTemplate.replace(new RegExp('!!!USER!!!', 'g'), process.env.USER!);
-fs.writeFileSync('www/index.html', index);
+// const indexTemplate = fs.readFileSync('index.template.html', 'utf-8');
+// const index = indexTemplate.replace(new RegExp('!!!USER!!!', 'g'), process.env.USER!);
+// fs.writeFileSync('www/index.html', index);
 
-const api = new apigateway.RestAPI("api", {
-    routes: [
-      {
-        path: "/",
-        localPath: "www"
-      },
-    ],
-  });
+// const api = new apigateway.RestAPI("api", {
+//     routes: [
+//       {
+//         path: "/",
+//         localPath: "www"
+//       },
+//     ],
+//   });
 
-export const url = api.url;
+// export const url = api.url;

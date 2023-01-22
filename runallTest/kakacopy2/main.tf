7e4e4ef6c7a095eb52bf7b8bd405bb820a946eb0
ItamarMalka <itamar.malka@env0.com>
2023-01-22 16:25:05 +0200

    move to modules folder

--- runallTest/kakacopy2/main.tf
+++
@@ -1,7 +0,0 @@
-module "module" {
-  source = "../resources"
-}
-
-output "null_resource_id" {
-  value = module.module.null_resource_id
-}
---
+++ runallTest/modules/kaka/main.tf
@@ -0,0 +1,8 @@
+module "module" {
+  source = "../resources"
+  trigger = "zero"
+}
+
+output "null_resource_id" {
+  value = module.module.null_resource_id
+}

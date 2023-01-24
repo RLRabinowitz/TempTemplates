d0b86437af02648a9795a94704d567ff1254f79c
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2023-01-24 18:17:13 +0200

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,3 +1,4 @@
-module "my" {
-  source = "git::https://gitlab.com/rlrabinowitz2/my-terraform-project?ref=arelll"
+module "aaaaa" {
+  source = "api-dev.dev.env0.com/9e4c5088-720d-4912-98a3-8b0c7e904a0f/aaaaa/ffff"
+  version = "0.0.1"
 }

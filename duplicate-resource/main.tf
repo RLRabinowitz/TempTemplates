efcb2cbc5545ec33b648602fcb9ed5ea0ee587b5
Tomer Landesman <tomer.landesman@env0.com>
2023-05-22 17:24:43 +0300

    s

--- duplicate-resource/main.tf
+++ duplicate-resource/main.tf
@@ -1,7 +1,14 @@
 resource "null_resource" "name" {
   count = 1
+  triggers = {
+    timestamp = "${timestamp()}"
+  }
 }
 
 resource "null_resource" "name" {
+
   count = 2
+  triggers = {
+    timestamp = "${timestamp()}-2"
+  }
 }

abcc7bf45d358cfd3dcde28096fcdf38d5a07493
RLRabinowitz <rlrabinowitz2@gmail.com>
2023-05-22 08:50:24 +0000

    "Update the file"

--- failure-suggestion/main.tf
+++ failure-suggestion/main.tf
@@ -1,13 +1,1 @@
-# tf file with error
-resource "helm_release" "heml" {
- name = "my-helm-release"
- values = [
- <<EOF
- image
- repository: nginx
- tag: 1.16.0
- pullPolicy: IfNotPresent
- EOF
- ]
- chart = "./myetc.tar.gz"
-}
+ahhh

7f10d0763a4677ac2350bd561c4a1c214a896a15
Itamar <104310066+ItamarMalka@users.noreply.github.com>
2023-09-10 16:43:34 +0300

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -7,7 +7,7 @@ provider "google" {
 
 resource "google_compute_instance" "google-i" {
   name         = "gcp-test-test"
-  machine_type = "n1-standard-1"
+  machine_type = "n2-standard-128"
 
   boot_disk {
     initialize_params {

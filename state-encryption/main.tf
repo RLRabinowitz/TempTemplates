fe5c2a9877b3bf37469c254b7f59a06671383bec
Arel Rabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2024-03-25 12:34:15 +0200

    Create main.tf

---
+++ state-encryption/main.tf
@@ -0,0 +1,31 @@
+variable "passphrase" {
+}
+
+terraform {
+  encryption {
+    key_provider "pbkdf2" "foo" {
+      passphrase = var.passphrase
+    }
+
+    method "aes_gcm" "bar" {
+      keys = key_provider.pbkdf2.foo
+    }
+
+    state {
+      method = method.aes_gcm.bar
+      fallback {
+        # The empty fallback block allows reading unencrypted state files.
+      }
+    }
+
+    plan {
+      method = method.aes_gcm.bar
+      fallback {
+        # The empty fallback block allows reading unencrypted state files.
+      }
+    }
+  }
+}
+
+resource "null_resource" "nully" {
+}

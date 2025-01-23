2f87ac8d4c103fe55adebb2a2f0b20db024a487f
Arel Rabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2025-01-23 22:20:43 +0200

    Update main.tf

--- more-modules/main.tf
+++ more-modules/main.tf
@@ -1,4 +1,4 @@
 module "bbb" {
-  source = "api-dev.dev.env0.com/9e4c5088-720d-4912-98a3-8b0c7e904a0f/bbb/ccc"
+  source = "api-pr18029.dev.env0.com/8001e07f-abcf-4e59-8d66-f35dd1969cdb/bbb/ccc"
   version = "1.0.0"
 }

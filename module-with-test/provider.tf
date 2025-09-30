1743313cb1ce188211ee325467ca3d331529242e
Arel Rabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2025-09-30 17:43:21 +0300

    Create provider.tf

---
+++ module-with-test/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    env0 = {
+      source = "env0/env0"
+      version = ">= 1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}

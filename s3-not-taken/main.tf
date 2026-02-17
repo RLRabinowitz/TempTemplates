a41f5ee31c0349d4fec8d31d9f18f5940ccb223d
Arel Rabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2026-02-17 17:49:38 +0200

    Update main.tf

--- s3-not-taken/main.tf
+++ s3-not-taken/main.tf
@@ -11,6 +11,19 @@ resource "aws_s3_bucket" "mine" {
   
 }
 
+resource "aws_s3_bucket" "mine" {
+  bucket = "arel-hackaton-ai-${var.suffix}-2"
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    MyTag = "Taggy"
+  }
+  
+}
+
 variable "suffix" {
   
 }
